4058ec5ae452ebd5d318f567b1b92bd07467c83e
Nolan <nolan.scobie@gmail.com>
2015-09-07 21:00:26 -0400

    Atlas: Should hopefully print results from ring touch sensors to DS

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Summer2015/AtlasTeleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Summer2015/AtlasTeleop.java
@@ -1,7 +1,10 @@
+//I know this is gross. We'll write pretty code when we have time.
+
 package com.qualcomm.ftcrobotcontroller.opmodes.Summer2015;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.ashebots.ftcandroidlib.drive.ChassisArcade;
 
@@ -14,6 +17,9 @@ public class AtlasTeleop extends OpMode
     DcMotor motorDriveRight;
     DcMotor motorLinearSlideLift;
 
+    TouchSensor touchRingFront;
+    TouchSensor touchRingBack;
+
     ChassisArcade chassis;
 
     @Override
@@ -21,10 +27,11 @@ public class AtlasTeleop extends OpMode
         motorDriveLeft = hardwareMap.dcMotor.get("motorDriveLeft");
         motorDriveLeft.setDirection(DcMotor.Direction.REVERSE);
         motorDriveRight = hardwareMap.dcMotor.get("motorDriveRight");
-        motorLinearSlideLift = hardwareMap.dcMotor.get("motorLinearSlideLift");
 
         motorLinearSlideLift = hardwareMap.dcMotor.get("motorLinearSlideLift");
 
+        touchRingFront = hardwareMap.touchSensor.get("touchRingFront");
+        touchRingBack = hardwareMap.touchSensor.get("touchRingBack");
 
         chassis = new ChassisArcade(motorDriveLeft, motorDriveRight);
     }
@@ -36,8 +43,23 @@ public class AtlasTeleop extends OpMode
 
         motorLinearSlideLift.setPower(gamepad1.right_stick_y);
 
+
+        telemetry.addData("touchRingFrontPressed", "Front");
+
         telemetry.addData("motorDriveLeftEncoder", "Left Drive Encoder: " + motorDriveLeft.getCurrentPosition());
         telemetry.addData("motorDriveRightEncoder", "Right Drive Encoder: " + motorDriveRight.getCurrentPosition());
+    }
 
+    private void telemetryUpdate()
+    {
+        String touchRingFrontString = (touchRingFront.isPressed()) ? "Front Ring: HEAVY" : "Front Ring: LIGHT";
+        telemetry.addData("touchRingFrontPressed", touchRingFrontString);
+
+        String touchRingBackString = (touchRingBack.isPressed()) ? "Back Ring: HEAVY" : "Back Ring: LIGHT";
+        telemetry.addData("touchRingBackPressed", touchRingBackString);
+
+
+        telemetry.addData("motorDriveLeftEncoder", "Left Drive Encoder: " + motorDriveLeft.getCurrentPosition());
+        telemetry.addData("motorDriveRightEncoder", "Right Drive Encoder: " + motorDriveRight.getCurrentPosition());
     }
 }
