9aeb1ffa99c5d66ffadf347ef32976045a462ddd
Nolan <nolan.scobie@gmail.com>
2015-09-10 17:05:08 -0400

    Beacon: Untested arm movement

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Summer2015/BeaconTeleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Summer2015/BeaconTeleop.java
@@ -5,6 +5,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes.Summer2015;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.ashebots.ftcandroidlib.drive.ChassisOmni;
 
@@ -18,9 +19,12 @@ public class BeaconTeleop extends OpMode
     DcMotor motorDriveFront;
     DcMotor motorDriveBack;
 
+    ChassisOmni chassis;
+
+    DcMotor motorArm;
+    Servo servoWrist;
     LightSensor forceRing;
 
-    ChassisOmni chassis;
 
     @Override
     public void init()
@@ -32,9 +36,11 @@ public class BeaconTeleop extends OpMode
         motorDriveBack = hardwareMap.dcMotor.get("motorDriveBack");
         motorDriveBack.setDirection(DcMotor.Direction.REVERSE);
 
-        forceRing = hardwareMap.lightSensor.get("forceRing");
-
         chassis = new ChassisOmni(motorDriveLeft, motorDriveRight, motorDriveFront, motorDriveBack);
+
+        motorArm = hardwareMap.dcMotor.get("motorArm");
+        servoWrist = hardwareMap.servo.get("servoWrist");
+        forceRing = hardwareMap.lightSensor.get("forceRing");
     }
 
     @Override
@@ -42,7 +48,20 @@ public class BeaconTeleop extends OpMode
     {
         try
         {
+            //DRIVE
             double leftDrivePower = chassis.Drive(gamepad1.left_stick_x, gamepad1.left_stick_y * -1.0f, gamepad1.right_stick_x);
+
+            //ARM
+            float armPower = gamepad1.left_trigger;
+            if (gamepad1.left_bumper == true) //The bumper inverts the direction of the trigger
+            {
+                armPower *= -1;
+            }
+            motorArm.setPower(armPower);
+
+            //WRIST
+
+            //TELEMETRY
             telemetry.addData("4", "leftDrivePower: " + leftDrivePower);
             telemetryUpdate();
         }
