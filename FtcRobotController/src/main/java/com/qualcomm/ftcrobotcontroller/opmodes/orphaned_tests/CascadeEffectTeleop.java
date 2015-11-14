948fc40cd857faf3ef3387fb313b0afe2409caca
Nolan <nolan.scobie@gmail.com>
2015-11-13 20:18:05 -0500

    Moved some example programs and fixed package declarations for others.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/orphaned_tests/CascadeEffectTeleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/orphaned_tests/CascadeEffectTeleop.java
@@ -1,5 +1,5 @@
 
-package com.qualcomm.ftcrobotcontroller.opmodes;
+package com.qualcomm.ftcrobotcontroller.opmodes.orphaned_tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/res_q/ResQRobot.java
@@ -0,0 +1,7 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.res_q;
+
+/**
+ * Created by Bill on 11/13/2015.
+ */
+public class ResQRobot {
+}
