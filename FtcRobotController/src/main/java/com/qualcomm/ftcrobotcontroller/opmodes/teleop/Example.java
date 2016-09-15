639a8a3e7ffe95be49e626eedab82ff15c49eeb7
artschell <arthurlschell@gmail.com>
2016-09-15 14:39:23 -0400

    Removed Past directory and ComplexOps
    Moved ComplexOps to AB_FtcAndroidLib. Removed Past directory.
    Note: Past directory is backed up on my computer. If we need it, it
    isn’t deleted. :) It was cluttering up the program and we really
    shouldn’t use last year’s work without releasing it.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/teleop/Example.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/teleop/Example.java
@@ -1,23 +1,24 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.teleop;
 
-import com.qualcomm.ftcrobotcontroller.opmodes.complexOps.*;
+import org.ashebots.ftcandroidlib.complexOps.*;
 
 /**
  * Created by apple on 9/6/16.
  */
 public class Example extends AdvOpMode {
+    AdvMotor m;
+    Chassis c;
     public void init() {
 
-        TriggerEvent b = new TriggerEvent();
-
-        String state = b.parse(gamepad1.left_trigger);
-
-        state = "UNPRESSED"; //The button is not pressed
-        state = "PRESSED";   //The button was just pressed
-        state = "HELD";      //The button is continuing to be pressed
-        state = "RELEASED";  //The button was just released
     }
+    BoolEvent b;
     public void loop() {
+        m.mRange(1000,INF);
 
+        c.mRange(1000,INF);
+        c.lRange(1000,INF);
+        c.rRange(1000,INF);
+    }
+    public void stop() {
     }
 }
